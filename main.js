ce96d076819f6f5f72b78609e981589ea18bb176
aswinkumarVP <93077603+aswinkumarVP@users.noreply.github.com>
2022-11-13 11:04:35 +0530

    second

---
+++ main.js
@@ -0,0 +1,27 @@
+const quize_question = [
+    {
+        question:"What is capacitor?",
+        options:[ "a device","a circuit breaker","simiconductor","none of the above"]
+    },
+    {
+        question:"What is capacitor?",
+        options:[ "a device","a circuit breaker","simiconductor","none of the above"]
+    },
+    {
+        question:"What is capacitor?",
+        options:[ "a device","a circuit breaker","simiconductor","none of the above"]
+    },
+    {
+        question:"What is capacitor?",
+        options:[ "a device","a circuit breaker","simiconductor","none of the above"]
+    },
+    {
+        question:"What is capacitor?",
+        options:[ "a device","a circuit breaker","simiconductor","none of the above"]
+    }
+]
+const quize_answer = [0,1,2,3,3]
+
+function startQuiz(){
+    let element = document.getElementById()
+}
